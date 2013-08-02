8792b53e8d9beb1daec7f238f2c4cfc857518936
Daniel Röwenstrunk <roewenstrunk@edirom.de>
2013-08-02 16:31:34 +0200

    Default window size 2/3 of width

--- EdiromOnline/app/view/desktop/Desktop.js
+++ EdiromOnline/app/view/desktop/Desktop.js
@@ -207,7 +207,7 @@ Ext.define('de.edirom.online.view.desktop.Desktop' ,{
         var me = this;
         var usableSize = me.getUsableSize();
 
-        var width = Math.max(300, (usableSize.width / 2) - 20);
+        var width = Math.max(300, (usableSize.width / 3 * 2) - 20);
         var height = Math.max(300, usableSize.height - 20);
         
         var position = [10, 5];
