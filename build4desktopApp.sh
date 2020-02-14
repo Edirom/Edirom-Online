8ffb0897124163a80debb2b38662f0bf8c4a1e14
Nikolaos Beer <nikolaos.beer@uni-paderborn.de>
2020-02-14 14:55:35 +0100

    Prepare app contents to be used in OPERA electron app

---
+++ build4desktopApp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -R ./build4desktopApp
+
+mkdir ./build4desktopApp
+
+cp -R ./app ./build4desktopApp
+cp -R ./data ./build4desktopApp
+cp -R ./ext-ux ./build4desktopApp
+cp -R ./extjs ./build4desktopApp
+cp -R ./help ./build4desktopApp
+cp -R ./index ./build4desktopApp
+cp -R ./main ./build4desktopApp
+cp -R ./modules ./build4desktopApp
+cp -R ./resources ./build4desktopApp
+cp -R ./scripts ./build4desktopApp
+cp -R ./*.xml ./build4desktopApp
+cp -R ./*.js ./build4desktopApp
+cp -R ./liav.html ./build4desktopApp
+cp -R ./*.xql ./build4desktopApp
+cp -R ./*.xconf ./build4desktopApp
