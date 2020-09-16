1e63011e3c157d69af40593ea75b813929ceac84
Nikolaos Beer <beer@max-reger-institut.de>
2020-09-16 16:39:30 +0200

    Test: do not load images for views that are not used

--- app/view/window/AnnotationView.js
+++ app/view/window/AnnotationView.js
@@ -442,8 +442,9 @@ Ext.define('EdiromOnline.view.window.AnnotationView', {
         });
 
         me.setPreviewGrid(participants);
-        me.setPreviewSingle(participants);
-        me.setPreviewList(participants);
+        // PB: Probably not needed when we only use grid view
+        // me.setPreviewSingle(participants);
+        // me.setPreviewList(participants);
     },
 
     setPreviewGrid: function(participants) {
@@ -480,6 +481,7 @@ Ext.define('EdiromOnline.view.window.AnnotationView', {
             var source = participant['source'];
             var siglum = participant['siglum'];
             var part = participant['part'];
+            // modifications for single-use-serve
             // var digilibBaseParams = participant['digilibBaseParams'];
             // var digilibSizeParams = participant['digilibSizeParams'];
             var digilibURL = participant['digilibURL'];
