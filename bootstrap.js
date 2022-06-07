1d822d93d23c718a47f48f8657e9e96a122e9080
bwbohl <b.w.bohl@gmail.com>
2022-06-08 01:39:09 +0200

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -506,6 +506,7 @@ Ext.ClassManager.addNameAlternateMappings({
   ],
   "Ext.grid.RowEditor": [],
   "Ext.grid.RowEditorButtons": [],
+  "Ext.grid.Scroller": [],
   "Ext.grid.View": [],
   "Ext.grid.ViewDropZone": [],
   "Ext.grid.column.Action": [
@@ -1514,6 +1515,7 @@ Ext.ClassManager.addNameAliasMappings({
   "Ext.grid.RowEditorButtons": [
     "widget.roweditorbuttons"
   ],
+  "Ext.grid.Scroller": [],
   "Ext.grid.View": [
     "widget.gridview"
   ],
