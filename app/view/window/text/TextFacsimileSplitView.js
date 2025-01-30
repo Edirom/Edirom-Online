bb7a18014a5d3633c5624eee6f80161818baf300
Daniel Röwenstrunk <roewenstrunk@uni-paderborn.de>
2025-01-30 14:54:01 +0100

    Default for annotations in texts should be inline display

--- app/view/window/text/TextFacsimileSplitView.js
+++ app/view/window/text/TextFacsimileSplitView.js
@@ -259,7 +259,9 @@ Ext.define('EdiromOnline.view.window.text.TextFacsimileSplitView', {
         var me = this;
         var annos = Ext.query('#' + me.id + '_textCont span.annotation');
         Ext.Array.each(annos, function(anno) {
-            Ext.get(anno).hide();
+            var a = Ext.get(anno);
+            a.setVisibilityMode(Ext.Element.DISPLAY);
+            a.hide();
         });
     },
 
